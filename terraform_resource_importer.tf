5b3400e7c9636023e6713805f516f83970bc4fa6
charanjit-dev <charanjit.singh@outlook.in>
2025-02-07 05:04:49 +0530

    Value updated for testing

--- terraform_resource_importer.tf
+++ terraform_resource_importer.tf
@@ -4,8 +4,8 @@ resource "aws_security_group" "existing_sg" {
   vpc_id      = var.vpcid # The VPC ID is parameterized for security reasons. In your code, either replace it here with your actual VPC ID or provide it in a *.tfvars file, as I have done.
 
   ingress {
-    from_port   = 4406
-    to_port     = 4406
+    from_port   = 3306
+    to_port     = 3306
     protocol    = "tcp"
     cidr_blocks = [var.cidr_block] # Referencing the parameterized CIDR block due to security reasons. Replace it with your actual CIDR block or provide it in a *.tfvars file.
   }
